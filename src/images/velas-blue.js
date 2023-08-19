7db508e745f671ac4e7560f4d2e343e109230987
Stefan C <admin@exzocoin.com>
2023-08-19 17:46:35 -0400

    Update velas-blue.js

--- src/images/velas-blue.js
+++ src/images/velas-blue.js
@@ -2,8 +2,31 @@ import * as React from 'react';
 
 const VelasIcon = (props) => {
   return (
-    <svg width={props.width} height={props.height} viewBox="0 0 45 38" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M22.0525 28.4477L13.7762 14.2238H30.3049L22.0525 28.4477ZM38.5706 9.472H5.51047L22.0525 37.9302L38.5706 9.48256V9.472ZM0 0L2.75523 4.75184H41.3258L44.0811 0H0Z" fill="#007AFF"/>
+    <svg width="41" height="47" viewBox="0 0 41 47" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path opacity="0.1" fill-rule="evenodd" clip-rule="evenodd" d="M18.8937 35.5948C16.9062 35.5948 14.9833 35.3248 13.1572 34.8171C13.3725 37.0672 13.803 39.2993 14.4487 41.4846C21.1216 39.8754 27.1953 36.3724 31.9201 31.3754L31.8196 31.271C28.2177 33.9855 23.7441 35.5948 18.8901 35.5948H18.8937Z" fill="black"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M30.4192 5.87545L20.2808 0C17.321 3.58575 15.0968 7.67193 13.6797 12.0137C21.479 13.6482 28.3707 17.7632 33.4937 23.5054C36.5323 26.9112 38.9468 30.8857 40.5612 35.2491V11.7509H40.5576L30.4228 5.87545H30.4192Z" fill="url(#paint0_linear_562_16149)"/>
+<path opacity="0.3" fill-rule="evenodd" clip-rule="evenodd" d="M17.1982 4.34281C17.2199 4.30667 17.2452 4.27052 17.2669 4.23438L17.1982 4.34281Z" fill="#A4A4A4"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M13.6685 34.9796C9.21281 35.9192 4.57412 36.0308 0 35.2496L10.142 41.125L20.284 47.0005L30.4224 41.125L40.5644 35.2496C38.95 30.8826 36.5356 26.908 33.4969 23.5059C28.1873 29.4677 21.188 33.3991 13.6757 34.9796H13.6685Z" fill="url(#paint1_linear_562_16149)"/>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M0 11.7512V35.253C4.57412 36.0342 9.21282 35.919 13.6685 34.983C11.1788 27.3867 11.2864 19.3403 13.6757 12.0176C15.0964 7.67584 17.3171 3.58966 20.2768 0.00390625L10.1348 5.87936L0 11.7512Z" fill="url(#paint2_linear_562_16149)"/>
+<path opacity="0.3" fill-rule="evenodd" clip-rule="evenodd" d="M5.59386 20.4633C4.26647 25.4279 3.97229 30.6589 4.7472 35.7423C7.75356 35.8539 10.7492 35.5947 13.6694 34.9791C11.1797 27.3828 11.2873 19.3364 13.6766 12.0137C15.0973 7.67193 17.318 3.58575 20.2777 0C12.8156 4.83501 7.79661 12.2189 5.59386 20.4633Z" fill="url(#paint3_linear_562_16149)"/>
+<defs>
+<linearGradient id="paint0_linear_562_16149" x1="44.9272" y1="31.631" x2="16.1089" y2="-0.104593" gradientUnits="userSpaceOnUse">
+<stop stop-color="white"/>
+<stop offset="1" stop-color="#D5D5D5"/>
+</linearGradient>
+<linearGradient id="paint1_linear_562_16149" x1="43.3662" y1="29.7233" x2="-0.877776" y2="44.4662" gradientUnits="userSpaceOnUse">
+<stop stop-color="#D4D4D4"/>
+<stop offset="1" stop-color="#F9F9F9"/>
+</linearGradient>
+<linearGradient id="paint2_linear_562_16149" x1="0.914824" y1="39.8468" x2="15.6468" y2="-0.888361" gradientUnits="userSpaceOnUse">
+<stop stop-color="white"/>
+<stop offset="1" stop-color="#D4D4D4"/>
+</linearGradient>
+<linearGradient id="paint3_linear_562_16149" x1="12.301" y1="0" x2="12.301" y2="35.7676" gradientUnits="userSpaceOnUse">
+<stop stop-color="#DFDFDF"/>
+<stop offset="1" stop-color="#A4A4A4" stop-opacity="0.49"/>
+</linearGradient>
+</defs>
 </svg>
   );
 };
